a1bfd1374937e04e934c3312090cde92517655b9
Jen Lindner <jenlindner22@gmail.com>
2019-10-29 20:30:12 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
 class Shoe
-  attr_accessor :brand, :color, :size, :material, :condition
-  attr_reader :title
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
  
   def initialize(title)
     @title = title
